c4c7beb483174f41905a60c965e9705c8c1f0e9f
feprevot <felixprevot@gmail.com>
2026-04-10 18:24:27 +0200

    feat: add reqiorement feat: init describe methode

--- p1/describe.py
+++ p1/describe.py
@@ -1,1 +1,19 @@
-q
+import os 
+import pandas as pd
+
+
+def describe(DATASET):
+    """Describe the dataset."""
+
+    df = pd.read_csv(DATASET)
+    print(df.head())
+
+def main():
+    if len(os.sys.argv) < 2:
+        print("Usage: python describe.py <dataset_path>")
+        return
+    dataset = os.sys.argv[1]
+    describe(dataset)
+
+if __name__ == "__main__":
+    main()
